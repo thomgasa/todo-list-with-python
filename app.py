0233a78f27dfe536fbd6145b297a02e5943e358f
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-01-07 18:42:51 -0400

    segundo commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+
 todos = []
 stop = False
 
@@ -7,24 +9,57 @@ def get_todos():
 
 def add_one_task(title):
     # your code here
+    """ Doc-string
+    esta función agrega el título de la tarea escrita
+    por el usuario y la agrega a la variable todos
+    """
+    todos.append(title)
+    print(todos)
     pass
 
 def print_list():
+     """ Doc-string
+    esta función imprime en consola las tareas guardadas
+    en la variable todos, sólo debería usarse como control
+    """
     global todos
-    pass
-
+    print(todos)
+    
 def delete_task(number_to_delete):
+    """ Doc-string
+    esta función elimina la tarea posicionada en el lugar
+    que el usuario elija
+    """
     # your code here
-    pass
+    todos.pop(int(number_to_delete)-1)
 
 def save_todos():
+    """ Doc-string
+    esta función crea/escribe un documento todos.csv
+    en el cual agrega las tareas delimitadas por ,
+    """
     # your code here
-    pass
-
+    with open('todos.csv', 'w', newline='\n') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(todos)
     
 def load_todos():
     # your code here
-    pass
+    """ Doc-string
+    esta función carga la lista de tareas en el .csv
+    luego limpia la variable (todos) y agrega en los valores
+    de la lista todos, finalmente imprime la lista en 
+    consola pero esta vez delimitada por líneas nuevas.
+    Esta es la función que debería usar el usuario común, 
+    dónde además verifica que el archivo .csv está correcto
+    """
+    with open('todos.csv', newline='\n') as csvfile:
+        reader=csv.reader(csvfile)
+        list.clear(todos)
+        for row in reader:
+            print('\n'.join(row))
+            todos.append(row)
+    
 
 # Below this code will only run if the entry file running was app.py
 if __name__ == '__main__':
